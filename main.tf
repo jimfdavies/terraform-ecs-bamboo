aa0b749a0b1c3fde3b7506aaba4014fe678b9d98
Jim Davies <jimfdavies@googlemail.com>
2017-09-22 09:47:34 +0100

    Add subnets, route table and route assoc

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ provider "aws" {
 }
 
 ## EC2
-
 ### Network
 
 data "aws_availability_zones" "available" {}
@@ -18,11 +17,33 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   count             = "${var.az_count}"
-  cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 10, count.index)}"
+  cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 2, count.index)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id            = "${aws_vpc.main.id}"
+  tags {
+    Name = "${aws_vpc.main.tags.Name}-${count.index}"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
+  tags {
+    Name = "${aws_vpc.main.tags.Name}"
+  }
+}
+
+#### Note: Public subnets only
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = "${var.az_count}"
+  subnet_id      = "${element(aws_subnet.main.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
 }
